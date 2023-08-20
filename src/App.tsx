e06d1da4fd40b03281c9b1a63b39e5795ee13a5b
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-08-20 15:58:20 +0800

    Header.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -3,11 +3,13 @@ import "@fontsource/roboto/700.css";
 
 import "./styles/styles.scss";
 import GoogleSearch from "./components/MainSearch";
+import Header from "./components/Header";
 import Footer from "./components/Footer";
 
 function App() {
   return (
     <>
+      <Header />
       <GoogleSearch />
       <Footer />
     </>
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,3 +1,39 @@
-const Header = () => {};
+import Profile from "../assets/avatar.jpg";
+
+import { SvgIcon, Tooltip } from "@mui/material";
+import Fade from "@mui/material/Fade";
+import Avatar from "@mui/material/Avatar";
+import { Stack } from "@mui/material";
+
+const Header = () => {
+  return (
+    <>
+      <Stack spacing={2} direction="row">
+        <Tooltip
+          title="Google apps"
+          TransitionComponent={Fade}
+          TransitionProps={{ timeout: 300 }}
+        >
+          <SvgIcon fontSize="large">
+            <svg className="gb_j" focusable="false" viewBox="0 0 24 24">
+              <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+            </svg>
+          </SvgIcon>
+        </Tooltip>
+        <Tooltip
+          title="Google Account Jemuel Repoylo syjem143@gmail.com"
+          TransitionComponent={Fade}
+          TransitionProps={{ timeout: 300 }}
+        >
+          <Avatar
+            alt="Jemuel Repoylo"
+            src={Profile}
+            sx={{ width: 34, height: 34 }}
+          />
+        </Tooltip>
+      </Stack>
+    </>
+  );
+};
 
 export default Header;
