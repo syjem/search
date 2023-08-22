6f0de42aac483b1587422208d5cc67dc46d69ead
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-08-22 22:27:18 +0800

    google search main page

---
+++ src/pages/GoogleSearchPage.tsx
@@ -0,0 +1,15 @@
+import GoogleSearch from "../components/MainSearch";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function Home() {
+  return (
+    <>
+      <Header />
+      <GoogleSearch />
+      <Footer />
+    </>
+  );
+}
+
+export default Home;
