e2527dd10b09a05f2189ee10dfde6ce5076bf3aa
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-08-22 22:26:09 +0800

    new footer nav

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,20 +4,30 @@ const Footer = () => {
   return (
     <footer className="footer">
       <Stack spacing={2}>
-        <h6>Philippines</h6>
-        <hr />
+        <Stack
+          direction="row"
+          justifyContent="space-between"
+          flexWrap="wrap"
+          gap={2}
+        >
+          <h6>Philippines</h6>
+          <h6 className="advance-search-nav">Advanced Search</h6>
+        </Stack>
+        <hr className="separator-hr" />
         <Stack
           direction="row"
           justifyContent="space-between"
           className="footer-nav-info"
+          flexWrap="wrap"
+          gap={5}
         >
-          <Stack direction="row" spacing={5}>
+          <Stack direction="row" flexWrap="wrap" gap={2}>
             <p>About</p>
             <p>Advertising</p>
             <p>Business</p>
             <p>How Search Work</p>
           </Stack>
-          <Stack direction="row" spacing={5}>
+          <Stack direction="row" spacing={3}>
             <p>Privacy</p>
             <p>Terms</p>
             <p>Settings</p>
