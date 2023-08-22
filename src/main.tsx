0a80c8361a775052a38b31ae14fcfa8efd10e43b
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-08-22 22:25:40 +0800

    add BrowserRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,17 @@
+import "@fontsource/roboto/400.css";
+import "@fontsource/roboto/700.css";
+
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App.tsx";
-import "./styles/_globals.scss";
+import { BrowserRouter } from "react-router-dom";
+
+import "./scss/_globals.scss";
 
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
   </React.StrictMode>
 );
