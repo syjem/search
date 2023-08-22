ff862ae283cba4e8320c64ab4412d6fa1d24f5e3
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-08-22 22:26:39 +0800

    add Link to routes

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -4,35 +4,47 @@ import { SvgIcon, Tooltip } from "@mui/material";
 import Fade from "@mui/material/Fade";
 import Avatar from "@mui/material/Avatar";
 import { Stack } from "@mui/material";
+import { Link } from "react-router-dom";
 
 const Header = () => {
   return (
-    <>
-      <Stack spacing={2} direction="row">
-        <Tooltip
-          title="Google apps"
-          TransitionComponent={Fade}
-          TransitionProps={{ timeout: 300 }}
-        >
-          <SvgIcon fontSize="large">
-            <svg className="gb_j" focusable="false" viewBox="0 0 24 24">
-              <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
-            </svg>
-          </SvgIcon>
-        </Tooltip>
-        <Tooltip
-          title="Google Account Jemuel Repoylo syjem143@gmail.com"
-          TransitionComponent={Fade}
-          TransitionProps={{ timeout: 300 }}
-        >
-          <Avatar
-            alt="Jemuel Repoylo"
-            src={Profile}
-            sx={{ width: 34, height: 34 }}
-          />
-        </Tooltip>
-      </Stack>
-    </>
+    <header className="header">
+      <nav className="nav">
+        <ul className="nav-menu">
+          <li className="nav-item gmail">Gmail</li>
+          <li className="nav-item images">
+            <Link to="/images">Images</Link>
+          </li>
+          <li className="nav-item">
+            <Stack spacing={2} direction="row" alignItems="center">
+              <Tooltip
+                title="Google apps"
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 300 }}
+              >
+                <SvgIcon fontSize="large" className="google-apps">
+                  <svg focusable="false" viewBox="0 0 24 24" fill="#ffffff">
+                    <path d="M6,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM6,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM12,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM16,6c0,1.1 0.9,2 2,2s2,-0.9 2,-2 -0.9,-2 -2,-2 -2,0.9 -2,2zM12,8c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,14c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2zM18,20c1.1,0 2,-0.9 2,-2s-0.9,-2 -2,-2 -2,0.9 -2,2 0.9,2 2,2z"></path>
+                  </svg>
+                </SvgIcon>
+              </Tooltip>
+              <Tooltip
+                title="Google Account Jemuel Repoylo syjem143@gmail.com"
+                TransitionComponent={Fade}
+                TransitionProps={{ timeout: 300 }}
+              >
+                <Avatar
+                  alt="Jemuel Repoylo"
+                  src={Profile}
+                  sx={{ width: 34, height: 34 }}
+                  className="avatar"
+                />
+              </Tooltip>
+            </Stack>
+          </li>
+        </ul>
+      </nav>
+    </header>
   );
 };
 
