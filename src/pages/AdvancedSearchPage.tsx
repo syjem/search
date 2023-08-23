41850dcbb0f79d9543039f53642dc5e8c7ba3f4e
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-08-23 13:45:31 +0800

    implement advanced search route

---
+++ src/pages/AdvancedSearchPage.tsx
@@ -0,0 +1,15 @@
+import AdvancedSearch from "../components/AdvancedSearch";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+
+function SearchAdvanced() {
+  return (
+    <>
+      <Header />
+      <AdvancedSearch />
+      <Footer />
+    </>
+  );
+}
+
+export default SearchAdvanced;
