89678d5b8a7310e3005f85e06a44b03438581fd0
Jemuel Repoylo <119649793+syjem@users.noreply.github.com>
2023-08-23 13:42:41 +0800

    render advanced search route

--- src/App.tsx
+++ src/App.tsx
@@ -3,12 +3,14 @@ import "./scss/styles.scss";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/GoogleSearchPage";
 import SearchImage from "./pages/ImageSearchPage";
+import SearchAdvanced from "./pages/AdvancedSearchPage";
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
       <Route path="/images" element={<SearchImage />} />
+      <Route path="/advanced_search" element={<SearchAdvanced />} />
     </Routes>
   );
 }
